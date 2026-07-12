ef73f0d6348b50b5d4b6a3a29900642195aa761e
princename16 <tinmanguygood@gmail.com>
2026-07-12 14:57:11 -0700

    Update Floor is Lava checksum

--- Formula/floorislava.rb
+++ Formula/floorislava.rb
@@ -3,7 +3,7 @@ class Floorislava < Formula
   homepage "https://github.com/princename16/Brew-Games"
   url "https://github.com/princename16/Brew-Games/archive/refs/heads/main.zip"
   version "1.0"
-  sha256 "SKIP"
+  sha256 "16b1fde70963439d8aaf3e21b54e7cbc5087432889ad0ef14469baa1bd71e195"
 
   def install
     bin.install "floorislava.py" => "floorislava"
