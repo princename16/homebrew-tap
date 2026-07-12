d11494c77ecf6031b030f9a557418c2bd7c62b53
princename16 <tinmanguygood@gmail.com>
2026-07-11 20:16:06 -0700

    Update hangman checksum

--- Formula/hangman.rb
+++ Formula/hangman.rb
@@ -3,7 +3,7 @@ class Hangman < Formula
   homepage "https://github.com/princename16/Brew-Games"
   url "https://github.com/princename16/Brew-Games/archive/refs/heads/main.zip"
   version "1.0"
-  sha256 "SKIP"
+  sha256 "1a7903f91947b39933285c357a5faf6d8e7a1212ddc0fdc08a0813445765dc8d"
 
   def install
     bin.install "hangman.py" => "hangman"
